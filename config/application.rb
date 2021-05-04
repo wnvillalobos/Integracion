8bf704cab7dc2cc3a8d71e6db9b9a10049a641d6
Wladimir Villalobos <wnvillalobos@gmail.com>
2021-05-04 05:53:53 -0400

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Tarea2Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    $path = 'https://apihost.com/'
+    $path = 'https://pure-dawn-05862.herokuapp.com/'
 
     # Configuration for the application, engines, and railties goes here.
     #
